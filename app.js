db0de547294f1795dfaf84b1bea5f1468713fc04
Gerardo Diaz <agustingerardo.diaz@gmail.com>
2022-10-10 14:50:14 -0400

    feat: add GET requests for READs on db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const db = require('./routes/postgres');
 
 app.use(bodyParser.json())
 app.use(
@@ -12,6 +13,10 @@ app.use(
 app.get('/', (req, res) => {
     res.json({info: "This is my app"});
 });
+
+app.get('/books', db.getBooks);
+
+app.get('/books/:id', db.getBooks);
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}...`);
