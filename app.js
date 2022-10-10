5ac45e37da0b5d045d0b08326bc979f564cd987e
Gerardo Diaz <agustingerardo.diaz@gmail.com>
2022-10-10 14:06:13 -0400

    feat: add GET request on root

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+/*
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+) */
+
+app.get('/', (req, res) => {
+    res.json({info: "This is my app"});
+});
+
 app.listen(port, () => {
     console.log(`Listening on port: ${port}...`);
 });
