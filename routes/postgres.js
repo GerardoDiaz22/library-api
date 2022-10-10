72114635d621c57a51dde03bec82173e0b8fd355
Gerardo Diaz <agustingerardo.diaz@gmail.com>
2022-10-10 15:53:57 -0400

    feat: add CREATE logic

--- routes/postgres.js
+++ routes/postgres.js
@@ -1,19 +1,19 @@
-const Pool = require('pg').Pool
+const Pool = require('pg').Pool;
 const pool = new Pool({
   user: 'gerardo',
   host: 'localhost',
   database: 'library',
   password: 'www',
   port: 5432,
-})
+});
 
 
 const getBooks = (req, res) => {
     pool.query('SELECT * FROM books INNER JOIN books_info USING(book_id) INNER JOIN authors USING(author_id) INNER JOIN images USING(image_id) ORDER BY book_id', (err, data) => {
         if (err) {
-            throw err
+            throw err;
         }
-        res.status(200).json(data.rows)
+        res.status(200).json(data.rows);
     })
 }
 
@@ -23,25 +23,52 @@ const getBookById = (req, res) => {
 
     pool.query('SELECT * FROM books INNER JOIN books_info USING(book_id) INNER JOIN authors USING(author_id) INNER JOIN images USING(image_id) WHERE book_id = $1 ORDER BY book_id', [id], (err, data) => {
         if (err) {
-            throw err
+            throw err;
         }
-        res.status(200).json(data.rows)
-    })
+        res.status(200).json(data.rows);
+    });
 }
+/*
+    -> POST book
+    INSERT INTO books(title, source) VALUES($1,$2) RETURNING *;
+    INSERT INTO authors(author) VALUES($1) RETURNING *;
+    INSERT INTO images(image_path) VALUES($1) RETURNING *;
+    INSERT INTO books_info(book_id, subtitle, category, publish_date, editors, description, author_id, image_id) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *;
+*/
 
+const createBook = (req, res) => {
+    const { title, authors, subtitle, category, publish_date, editors, description, image, source } = req.body;
+    
+    pool.query('INSERT INTO books (title, source) VALUES ($1, $2) RETURNING *', [title, source], (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const book_id = data.rows[0].book_id;
+    });
+
+    pool.query('INSERT INTO authors (author) VALUES ($1) RETURNING *', [authors], (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const author_id = data.rows[0].author_id;
+    });
+
+    pool.query('INSERT INTO images (image_path) VALUES ($1) RETURNING *', [image], (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const image_id = data.rows[0].image_id;
+    });
+
+    pool.query('INSERT INTO books_info(book_id, subtitle, category, publish_date, editors, description, author_id, image_id) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *', [book_id,subtitle,category, publish_date, editors, description, author_id, image_id], (err, data) => {
+        if (err) {
+            throw err;
+        }
+        res.status(200).send(`Book added with ID: ${data.rows[0].boo_info_id}`)
+    });
+}
 
 /*
-const createUser = (req, res) => {
-    const { name, email } = req.body
-  
-    pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email], (err, data) => {
-      if (err) {
-        throw err
-      }
-      res.status(201).send(`User added with ID: ${data.rows[0].id}`)
-    })
-}
-
 const updateUser = (req, res) => {
     const id = parseInt(req.params.id)
     const { name, email } = req.body
