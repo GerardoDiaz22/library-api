dff080aa5fb0d7edf29ea04de45307b372f2b3c8
Gerardo Diaz <agustingerardo.diaz@gmail.com>
2022-10-10 15:30:41 -0400

    fix: wrong bodyParser config

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const bodyParser = require('bodyParser');
 const db = require('./routes/postgres');
 const app = express();
 const port = 8080;
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({info: "This is my app"});
