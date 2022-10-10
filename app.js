0d31a7c2efbfc1d6102c55019c422a3b279810cc
Gerardo Diaz <agustingerardo.diaz@gmail.com>
2022-10-10 14:22:31 -0400

    feat: add body parser

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-/*
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-) */
+);
 
 app.get('/', (req, res) => {
     res.json({info: "This is my app"});
