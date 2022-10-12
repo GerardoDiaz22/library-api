92778fe9fcfcb6ca9309040f3aeb6cf3f121ffdd
Gerardo Diaz <agustingerardo.diaz@gmail.com>
2022-10-12 02:08:11 -0400

    feat: add dotenv file and config

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const db = require('./routes/postgres');
 const app = express();
+require('dotenv').config()
+
 const port = process.env.PORT || 8000;
 
 app.use(express.urlencoded({extended: true}));
