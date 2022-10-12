8018952009d5a5268bdc765368d83a7be0f4e6fe
Gerardo Diaz <agustingerardo.diaz@gmail.com>
2022-10-12 05:16:24 -0400

    feat: add browserify to bundle libs

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,13 +1,21 @@
 sap.ui.define(
-    ["./BaseController"],
+    ["./BaseController",],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
     function (Controller) {
         "use strict";
-
+        
         return Controller.extend("lib.app.libAPI.controller.MainView", {
-            onInit: function () {},
+            onInit: function () {
+                const oTable = this.getView().byId("bookshelf");
+                const oModel = new sap.ui.model.json.JSONModel();
+
+                //const books = database.getBooks;
+                
+                oTable.setModel(oModel);
+                //oModel.setData(books);
+            },
         });
     }
 );
--- uimodule/webapp/libs/postgres.js
+++ uimodule/webapp/libs/postgres.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   port: 5432,
 });
 
+<<<<<<< HEAD
 const axios = require('axios');
 
 const getBooks = (req, res, next) => {
@@ -124,12 +125,125 @@ const createBook = async(req, res, next) => {
     catch (err) {
         throw err;
     }
+=======
+
+const getBooks = (req, res) => {
+    pool.query('SELECT * FROM books INNER JOIN books_info USING(book_id) INNER JOIN authors USING(author_id) INNER JOIN images USING(image_id) ORDER BY book_id', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        res.status(200).json(data.rows);
+    })
+}
+
+
+const getBookById = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    pool.query('SELECT * FROM books INNER JOIN books_info USING(book_id) INNER JOIN authors USING(author_id) INNER JOIN images USING(image_id) WHERE book_id = $1 ORDER BY book_id', [id], (err, data) => {
+        if (err) {
+            throw err;
+        }
+        res.status(200).json(data.rows);
+    });
+}
+
+const createBook = (req, res) => {
+    const { title, authors, subtitle, category, publish_date, editors, description, image_path, source } = req.body;
+
+    pool.query('SELECT * FROM books WHERE title = $1', [title], (err, data) => {
+        if (err) {
+            throw err;
+        }
+        if (data.rows[0] != undefined) {
+            res.status(200).send(`Book already added with ID: ${data.rows[0].book_id}`);
+        }
+        else {
+            pool.query('INSERT INTO books (title, source) VALUES ($1, $2) RETURNING *', [title, source], (err, data) => {
+                if (err) {
+                    throw err;
+                }
+                const book_id = data.rows[0].book_id;
+                pool.query('SELECT * FROM authors WHERE author = $1', [authors], (err, data) => {
+                    if (err) {
+                        throw err;
+                    }
+                    if (data.rows[0] != undefined) {
+                        const author_id = data.rows[0].author_id;
+
+                        pool.query('SELECT * FROM images WHERE image_path = $1', [image_path], (err, data) => {
+                            if (data.rows[0] != undefined) {
+                                const image_id = data.rows[0].image_id;
+                                pool.query('INSERT INTO books_info(book_id, subtitle, category, publish_date, editors, description, author_id, image_id) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *', [book_id,subtitle,category, publish_date, editors, description, author_id, image_id], (err, data) => {
+                                    if (err) {
+                                        throw err;
+                                    }
+                                    res.status(200).send(`Book added with ID: ${data.rows[0].book_id}`);
+                                });
+                            }
+                            else {
+                                pool.query('INSERT INTO images (image_path) VALUES ($1) RETURNING *', [image_path], (err, data) => {
+                                    if (err) {
+                                        throw err;
+                                    }
+                                    const image_id = data.rows[0].image_id;
+                                    pool.query('INSERT INTO books_info(book_id, subtitle, category, publish_date, editors, description, author_id, image_id) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *', [book_id,subtitle,category, publish_date, editors, description, author_id, image_id], (err, data) => {
+                                        if (err) {
+                                            throw err;
+                                        }
+                                        res.status(200).send(`Book added with ID: ${data.rows[0].book_id}`);
+                                    });
+                                });
+                            }
+                        });
+                        
+                    }
+                    else {
+                        pool.query('INSERT INTO authors (author) VALUES ($1) RETURNING *', [authors], (err, data) => {
+                            if (err) {
+                                throw err;
+                            }
+                            const author_id = data.rows[0].author_id;
+                            pool.query('SELECT * FROM images WHERE image_path = $1', [image_path], (err, data) => {
+                                if (data.rows[0] != undefined) {
+                                    const image_id = data.rows[0].image_id;
+                                    pool.query('INSERT INTO books_info(book_id, subtitle, category, publish_date, editors, description, author_id, image_id) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *', [book_id,subtitle,category, publish_date, editors, description, author_id, image_id], (err, data) => {
+                                        if (err) {
+                                            throw err;
+                                        }
+                                        res.status(200).send(`Book added with ID: ${data.rows[0].book_id}`);
+                                    });
+                                }
+                                else {
+                                    pool.query('INSERT INTO images (image_path) VALUES ($1) RETURNING *', [image_path], (err, data) => {
+                                        if (err) {
+                                            throw err;
+                                        }
+                                        const image_id = data.rows[0].image_id;
+                                        pool.query('INSERT INTO books_info(book_id, subtitle, category, publish_date, editors, description, author_id, image_id) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *', [book_id,subtitle,category, publish_date, editors, description, author_id, image_id], (err, data) => {
+                                            if (err) {
+                                                throw err;
+                                            }
+                                            res.status(200).send(`Book added with ID: ${data.rows[0].book_id}`);
+                                        });
+                                    });
+                                }
+                            });
+                        });
+                    }
+                });
+            });
+        }
+    });
+    
+>>>>>>> e9d8bcc (feat: add browserify to bundle libs)
 }
 
 const updateBookById = (req, res) => {
     const id = parseInt(req.params.id);
     const { title, subtitle, category, publish_date, editors, description, authors, image_path } = req.body;
   
+<<<<<<< HEAD
     pool.query('UPDATE books SET title = $1 WHERE book_id = $2',[title, id])
     .then( () => {
         pool.query('UPDATE books_info SET subtitle = $1 WHERE book_id = $2',[subtitle, id])
@@ -185,6 +299,126 @@ const deleteBookById = (req, res, next) => {
     .catch( (err) => {
         throw err;
     });
+=======
+    pool.query(
+        'UPDATE books SET title = $1 WHERE book_id = $2',
+        [title, id],
+        (err, data) => {
+            if (err) {
+                throw err;
+            }
+        }
+    );
+
+    pool.query(
+        'UPDATE books_info SET subtitle = $1 WHERE book_id = $2',
+        [subtitle, id],
+        (err, data) => {
+            if (err) {
+                throw err;
+            }
+        }
+    );
+
+    pool.query(
+        'UPDATE books_info SET category = $1 WHERE book_id = $2',
+        [category, id],
+        (err, data) => {
+            if (err) {
+                throw err;
+            }
+        }
+    );
+
+    pool.query(
+        'UPDATE books_info SET publish_date = $1 WHERE book_id = $2',
+        [publish_date, id],
+        (err, data) => {
+            if (err) {
+                throw err;
+            }
+        }
+    );
+
+    pool.query(
+        'UPDATE books_info SET editors = $1 WHERE book_id = $2',
+        [editors, id],
+        (err, data) => {
+            if (err) {
+                throw err;
+            }
+        }
+    );
+
+    pool.query(
+        'UPDATE books_info SET description = $1 WHERE book_id = $2',
+        [description, id],
+        (err, data) => {
+            if (err) {
+                throw err;
+            }
+        }
+    );
+
+    pool.query(
+        'SELECT author_id FROM books_info WHERE book_id = $1',
+        [id],
+        (err, data) => {
+            if (err) {
+                throw err;
+            }
+            const author_id = data.rows[0].author_id;
+            pool.query(
+                'UPDATE authors SET author = $1 WHERE author_id = $2',
+                [authors,author_id],
+                (err, data) => {
+                    if (err) {
+                        throw err;
+                    }
+                }
+            );
+        }
+    );
+    
+    pool.query(
+        'SELECT image_id FROM books_info WHERE book_id = $1',
+        [id],
+        (err, data) => {
+            if (err) {
+                throw err;
+            }
+            const image_id = data.rows[0].image_id;
+            pool.query(
+                'UPDATE images SET image_path = $1 WHERE image_id = $2',
+                [image_path, image_id],
+                (err, data) => {
+                    if (err) {
+                        throw err;
+                    }
+                }
+            );
+        }
+    );
+    
+    res.status(200).send(`Book modified with ID: ${id}`);
+}
+
+
+const deleteBookById = (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    pool.query('DELETE FROM books_info WHERE book_id = $1', [id], (err, data) => {
+        if (err) {
+            throw err;
+        }
+        pool.query('DELETE FROM books WHERE book_id = $1', [id], (err, data) => {
+            if (err) {
+                throw err;
+            }
+            res.status(200).send(`Book deleted with ID: ${id}`);
+        });
+    })
+>>>>>>> e9d8bcc (feat: add browserify to bundle libs)
 }
 
 module.exports = {
@@ -193,4 +427,27 @@ module.exports = {
     createBook,
     updateBookById,
     deleteBookById
+<<<<<<< HEAD
 }
+=======
+}
+
+/*
+-> GET book by author
+SELECT * FROM books INNER JOIN books_info USING(book_id) INNER JOIN authors USING(author_id) INNER JOIN images USING(image_id) WHERE author = $1 ORDER BY book_id;
+
+-> GET book by category
+SELECT * FROM books INNER JOIN books_info USING(book_id) INNER JOIN authors USING(author_id) INNER JOIN images USING(image_id) WHERE category = $1 ORDER BY book_id;
+
+-> PUT att book by ID
+UPDATE books SET title = $1 WHERE book_id = $2;
+UPDATE books_info SET subtitle = $1 WHERE book_id = $2;
+UPDATE books_info SET category = $1 WHERE book_id = $2;
+UPDATE books_info SET publish_date = $1 WHERE book_id = $2;
+UPDATE books_info SET editors = $1 WHERE book_id = $2;
+UPDATE books_info SET description = $1 WHERE book_id = $2;
+UPDATE books_info SET author_id = $1 WHERE book_id = $2;
+UPDATE books_info SET image_id = $1 WHERE book_id = $2;
+
+*/
+>>>>>>> e9d8bcc (feat: add browserify to bundle libs)
