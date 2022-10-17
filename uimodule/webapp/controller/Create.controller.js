28d6935429d3eda4f1ac75bdb72d71500d4ab78e
Gerardo Diaz <agustingerardo.diaz@gmail.com>
2022-10-17 16:43:03 -0400

    feat: add controller for form

--- uimodule/webapp/controller/Create.controller.js
+++ uimodule/webapp/controller/Create.controller.js
@@ -1,14 +1,45 @@
-sap.ui.define(
-    ["./BaseController"],
+sap.ui.define([
+    "./BaseController",
+    "sap/m/MessageToast"],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageToast) {
         "use strict";
         
         return Controller.extend("lib.app.libAPI.controller.Create", {
             onInit: function () {
                 
+            },
+            onCreatePress: function () {
+                console.log('Press...');
+                const title = this.getView().byId("bookTitle").getValue();
+                const subtitle = this.getView().byId("bookSubtitle").getValue();
+                const authors = this.getView().byId("bookAuthors").getValue();
+                const editors = this.getView().byId("bookEditors").getValue();
+                const category = this.getView().byId("bookCategory").getValue();
+                const publishDate = this.getView().byId("bookPublishDate").getValue();
+                const imagePath = this.getView().byId("bookImage").getValue();
+                const description = this.getView().byId("bookDescription").getValue();
+
+                $.ajax({
+                    url: "http://localhost:8000/books",
+                    method: 'POST',
+                    data: {
+                        "title": title,
+                        "source": "internal DB",
+                        "subtitle": subtitle,
+                        "authors": authors,
+                        "editors": editors,
+                        "category": category,
+                        "publish_date": '1999-10-10',
+                        "image_path": imagePath,
+                        "description": description
+                    }
+                })
+                .done((res) => {
+                    MessageToast.show(res);
+                });
             }
         });
     }
