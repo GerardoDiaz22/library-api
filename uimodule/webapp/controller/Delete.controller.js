d6a970f235c179bace2954ff6e1b118c78db38f2
Gerardo Diaz <agustingerardo.diaz@gmail.com>
2022-10-17 18:09:18 -0400

    feat: add delete book operation

--- uimodule/webapp/controller/Delete.controller.js
+++ uimodule/webapp/controller/Delete.controller.js
@@ -1,14 +1,26 @@
-sap.ui.define(
-    ["./BaseController"],
+sap.ui.define([
+    "./BaseController",
+    "sap/m/MessageToast"],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageToast) {
         "use strict";
         
         return Controller.extend("lib.app.libAPI.controller.Delete", {
             onInit: function () {
                 
+            },
+            onDeletePress: function () {
+                const bookID = this.getView().byId("bookID").getValue();
+
+                $.ajax({
+                    url: `http://localhost:8000/books/${bookID}`,
+                    method: 'DELETE'
+                })
+                .done((res) => {
+                    MessageToast.show(res);
+                });
             }
         });
     }
