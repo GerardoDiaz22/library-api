dea55b56998e7fe7232f2f692bd46a185a73f9dd
Gerardo Diaz <agustingerardo.diaz@gmail.com>
2022-10-19 16:10:22 -0400

    feat: add toolbar controller

--- uimodule/webapp/controller/Get.controller.js
+++ uimodule/webapp/controller/Get.controller.js
@@ -2,15 +2,58 @@ sap.ui.define([
     "./BaseController",
     "sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
-    "sap/ui/model/json/JSONModel"],
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, Filter, FilterOperator, JSONModel) {
+    function (Controller, Filter, FilterOperator, JSONModel, MessageToast) {
         "use strict";
         
         return Controller.extend("lib.app.libAPI.controller.Get", {
             onInit: function () {
+
+                const oModelSelect = new JSONModel();
+                this.getView().byId('selectMenu').setModel(oModelSelect);
+
+                const oData = {
+                    "SelectedAttribute": "title",
+                    "Attributes": [
+                        {
+                            "AttrId": "title",
+                            "Name": "Title"
+                        },
+                        {
+                            "AttrId": "subtitle",
+                            "Name": "Subtitle"
+                        },
+                        {
+                            "AttrId": "authors",
+                            "Name": "Author"
+                        },
+                        {
+                            "AttrId": "editors",
+                            "Name": "Editor"
+                        },
+                        {
+                            "AttrId": "category",
+                            "Name": "Category"
+                        },
+                        {
+                            "AttrId": "description",
+                            "Name": "Description"
+                        },
+                        {
+                            "AttrId": "source",
+                            "Name": "Source"
+                        }
+                    ],
+                    "Editable": true,
+                    "Enabled": true
+                };
+                
+                oModelSelect.setData(oData);
+                
                 const oModel = new JSONModel();
                 this.getView().setModel(oModel);
                 
@@ -20,25 +63,44 @@ sap.ui.define([
                     dataType: 'json'
                 })
                 .done((res) => {
+                    if (Object.keys(res).length === 0){
+                        MessageToast.show("Not found");
+                    }
                     oModel.setData(res);
                 });
             },
             onRefreshPress: function () {
-                //TODO: refresh table alone
-                location.reload();
-            },
-            onSearch: function (oEvent) {
-                const sQuery = oEvent.getSource().getValue();
                 const oModel = new JSONModel();
                 this.getView().setModel(oModel);
                 $.ajax({
-                    url: `http://localhost:8000/books?title=${sQuery}`,
+                    url: "http://localhost:8000/books",
                     type: 'GET',
                     dataType: 'json'
                 })
                 .done((res) => {
+                    if (Object.keys(res).length === 0){
+                        MessageToast.show("Not found");
+                    }
                     oModel.setData(res);
                 });
+            },
+            onSearch: function (oEvent) {
+                const sQuery = oEvent.getSource().getValue();
+                const sQueryType = this.getView().byId("selectMenu").getSelectedKey();
+                const oModel = new JSONModel();
+                this.getView().setModel(oModel);
+                $.ajax({
+                    url: `http://localhost:8000/books?${sQueryType}=${sQuery}`,
+                    type: 'GET',
+                    dataType: 'json'
+                })
+                .done((res) => {
+                    if (Object.keys(res).length === 0){
+                        MessageToast.show("Not found");
+                    }
+                    oModel.setData(res);
+                });
+
             }
         });
     }
